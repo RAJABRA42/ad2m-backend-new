c8eb59e8d017f830c427623748433dd7edfc49e8
RAJABRA42 <rybarisoa@gmail.com>
2026-01-22 11:31:08 +0000

    test

--- resources/js/pages/About.vue
+++ resources/js/pages/About.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <h1 style="color: red; font-size: 30px;">🔥 HOME VUE S’AFFICHE ! 🔥</h1>
+    <h1 style="color: red; font-size: 30px;">🔥 Barthelemy 🔥</h1>
     <p>Bravo Barthelemy, le routeur fonctionne.</p>
   </div>
 </template>
